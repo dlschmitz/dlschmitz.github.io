b08aee2647c8a9cc20f33dfe94d8f682a6dba271
dlschmitz <dlschmitz@gmail.com>
2020-05-12 14:58:24 -0700

    color and border improvements

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,18 +1,17 @@
+/*This pulls in the current date and time for me*/
 document.getElementById("year").textContent = new Date().getFullYear();
-
 var d = new Date();
 var date = d. getDate();
 var month = d. getMonth() + 1;
 var year = d. getFullYear();
 var dateStr = month + "/" + date + "/" + year;
-
 document.getElementById("CurrentTime").innerHTML = " " + dateStr + " " + new Date(new Date().getTime()).toLocaleTimeString();
 
 const responsebutton = document.querySelector('.response');
 const mainnav = document.querySelector('.navi')
 
 responsebutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
+/*This changes the language in the mobile view from Open Menu to Close Menu */
 var button = document.getElementById("Switch");
 button.addEventListener('click', 
 function() {
