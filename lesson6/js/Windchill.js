e05f93ab8d6e9e82b5953d8dd8457ef79b9e56a9
dlschmitz <dlschmitz@gmail.com>
2020-05-28 15:29:27 -0700

    continued attempts

--- lesson6/js/Windchill.js
+++ lesson6/js/Windchill.js
@@ -1,10 +1,14 @@
 var CurrentTemp = document.getElementById("Temp").value;
 var CurrentSpeed = document.getElementById("Speed").value;
 
-function WindChill (CurrentTemp, CurrentSpeed) {
+function Chill (CurrentTemp, CurrentSpeed) {
     var Result = 35.74 + 0.6215 * CurrentTemp - 35.75 * Math.pow(CurrentSpeed, 0.16) + 0.4275 * CurrentTemp * Math.pow(CurrentSpeed, 0.16);
-    
-    if (CurrentTemp < 51 && CurrentSpeed > 2.9) {
+    return(Result);
+}
+
+if (CurrentTemp < 51 && CurrentSpeed > 2.9) {
     document.getElementById("WindChill").textContent = Result;
 }
+else {
+    document.getElementById("WindChill").textContent = "N/A"
 }
