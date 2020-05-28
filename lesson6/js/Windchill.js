65f1b103601f9703619de63c4ad54c983a9fd421
dlschmitz <dlschmitz@gmail.com>
2020-05-28 00:08:01 -0700

    working on javascript formula

---
+++ lesson6/js/Windchill.js
@@ -0,0 +1,10 @@
+var CurrentTemp = document.getElementById("Temp").value;
+var CurrentSpeed = document.getElementById("Speed").value;
+var Result = 35.74 + 0.6215 * CurrentTemp - 35.75 * Math.pow(CurrentSpeed, 0.16) + 0.4275 * CurrentTemp * Math.pow(CurrentSpeed, 0.16);
+
+if (CurrentTemp < 51 && CurrentSpeed > 2.9) {
+    document.getElementById("WindChill").textContent = Result;
+}
+else {
+    document.getElementById("WindChill").textContent = "N/A";
+}
