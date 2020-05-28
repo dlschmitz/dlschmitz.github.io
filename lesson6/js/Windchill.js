ae7836faa44682e50aca8b345149cda7513ead44
dlschmitz <dlschmitz@gmail.com>
2020-05-28 16:50:27 -0700

    more updates

--- lesson6/js/Windchill.js
+++ lesson6/js/Windchill.js
@@ -3,9 +3,9 @@ let s = document.getElementById('Speed').textContent;
 
 if (t <= 50 && s >= 3) {
     let p = Math.pow(s, 0.16);
-    let f = 35.74 + 0.6215 * t - 35.75 * p + 0.4275 * t * p;
-    let r = f.toFixed(1) + '&#8457;';
-    document.getElementById('WindChill').textContent = r;
+    let f = 35.74 + 0.6215 * t - 35.75 - p + 0.4275 * t * p;
+    let result = f.toFixed(1);
+    document.getElementById('WindChill').textContent = result;
 } else {
     document.getElementById('WindChill').textContent = "N/A";
 }
