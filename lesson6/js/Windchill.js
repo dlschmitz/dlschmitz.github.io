8c35ccaf265ccda527f74bad49c254617835478e
dlschmitz <dlschmitz@gmail.com>
2020-05-28 16:44:49 -0700

    please work - more javascript

--- lesson6/js/Windchill.js
+++ lesson6/js/Windchill.js
@@ -2,7 +2,7 @@ let t = document.getElementById('Temp').textContent;
 let s = document.getElementById('Speed').textContent;
 
 if (t <= 50 && s >= 3) {
-    let p = Math.pow(s, 0.16)
+    let p = Math.pow(s, 0.16);
     let f = 35.74 + 0.6215 * t - 35.75 * p + 0.4275 * t * p;
     let r = f.toFixed(1) + '&#8457;';
     document.getElementById('WindChill').textContent = r;
