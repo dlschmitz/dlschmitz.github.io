3c3a037e269f9b2d03f0d9ce4c0701f28c603186
dlschmitz <dlschmitz@gmail.com>
2020-05-28 16:42:36 -0700

    please work this time - javascript

--- lesson6/js/Windchill.js
+++ lesson6/js/Windchill.js
@@ -1,11 +1,11 @@
-
 let t = document.getElementById('Temp').textContent;
 let s = document.getElementById('Speed').textContent;
 
 if (t <= 50 && s >= 3) {
-    let result = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    document.getElementById("WindChill").textContent = result;
-}
-else {
-    document.getElementById("WindChill").textContent = "N/A"
+    let p = Math.pow(s, 0.16)
+    let f = 35.74 + 0.6215 * t - 35.75 * p + 0.4275 * t * p;
+    let r = f.toFixed(1) + '&#8457;';
+    document.getElementById('WindChill').textContent = r;
+} else {
+    document.getElementById('WindChill').textContent = "N/A";
 }
