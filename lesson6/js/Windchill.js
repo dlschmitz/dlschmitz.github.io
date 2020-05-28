55db2f58d7ed7bc3dda37f95cbb92e1f7d66014b
dlschmitz <dlschmitz@gmail.com>
2020-05-28 16:30:46 -0700

    more java work

--- lesson6/js/Windchill.js
+++ lesson6/js/Windchill.js
@@ -1,13 +1,10 @@
-var CurrentTemp = document.getElementById("Temp").value;
-var CurrentSpeed = document.getElementById("Speed").value;
 
-function Chill (CurrentTemp, CurrentSpeed) {
-    var Result = 35.74 + 0.6215 * CurrentTemp - 35.75 * Math.pow(CurrentSpeed, 0.16) + 0.4275 * CurrentTemp * Math.pow(CurrentSpeed, 0.16);
-    return(Result);
-}
+let t = document.getElementById('Temp').textContent;
+let s = document.getElementById('Speed').textContent;
 
-if (CurrentTemp < 51 && CurrentSpeed > 2.9) {
-    document.getElementById("WindChill").textContent = Result;
+if (t <= 50 && s >= 3) {
+    let result = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    document.getElementById("WindChill").textContent = result;
 }
 else {
     document.getElementById("WindChill").textContent = "N/A"
