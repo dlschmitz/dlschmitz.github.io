e7355fd6df910524ea1b9b5433d35c2030893e4f
dlschmitz <dlschmitz@gmail.com>
2020-05-28 17:02:59 -0700

    works in codepen, but not here

--- lesson6/js/Windchill.js
+++ lesson6/js/Windchill.js
@@ -3,7 +3,7 @@ let s = document.getElementById('Speed').textContent;
 
 if (t <= 50 && s >= 3) {
     let p = Math.pow(s, 0.16);
-    let f = 35.74 + 0.6215 * t - 35.75 - p + 0.4275 * t * p;
+    let f = 35.74 + 0.6215 * t - 35.75 * p + 0.4275 * t * p;
     let result = f.toFixed(1);
     document.getElementById('WindChill').innerHTML = result + '&#8457;';
 } else {
