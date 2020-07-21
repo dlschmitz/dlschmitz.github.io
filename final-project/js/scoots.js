79c44535949a5a555174067f2cdab99ba9c61a4e
dlschmitz <dlschmitz@gmail.com>
2020-07-21 12:33:41 -0700

    completed tour page

--- final-project/js/scoots.js
+++ final-project/js/scoots.js
@@ -30,3 +30,32 @@ button.addEventListener('click',
             button.innerHTML = button.getAttribute("data-text-swap");
         }
     }, false);
+
+/*Lazy image loading*/
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
